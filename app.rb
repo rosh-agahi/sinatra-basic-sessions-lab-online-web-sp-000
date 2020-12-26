10e3ccec58dcbd8248bf568d43c086745e27b3d2
Roshanak Agahi <rosh.agahi@gmail.com>
2020-12-26 19:19:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,4 +5,16 @@ class App < Sinatra::Base
     enable :sessions
     set :session_secret, "secret"
   end 
+  
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    session[:item] = params[:item]
+    @session = session
+
+    erb :checkout
+  end
+  
 end
